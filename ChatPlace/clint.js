34ddabfd562fd0d89b3a27cb43a48c031eadd226
kuldeep <kuldeephawladar14@gmail.com>
2024-09-19 15:47:37 +0530

    added privacy page

--- ChatPlace/clint.js
+++ ChatPlace/clint.js
@@ -1,8 +1,8 @@
-const socket = io('http://localhost:8000');
+// const socket = io('http://localhost:8000');
 
-const form = document.getElementById("send-container");
-const messageInp = document.getElementById("messageInp");
-const messageContainer = document.querySelector(".container");
+// const form = document.getElementById("send-container");
+// const messageInp = document.getElementById("messageInp");
+// const messageContainer = document.querySelector(".container");
 
-const name = prompt("Enter your name to join a chat");
-socket.emit("new-user-joined", name);
+// const name = prompt("Enter your name to join a chat");
+// socket.emit("new-user-joined", name);
